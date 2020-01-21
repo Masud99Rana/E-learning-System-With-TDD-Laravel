9a5bd53270bb048d8523c90c04ea03332f9c06f7
Masud99Rana <Masudrana917775@gmail.com>
2020-01-21 09:30:15 +0600

    Series data caching using redis

--- app/Entities/Learning.php
+++ app/Entities/Learning.php
@@ -16,4 +16,60 @@ trait Learning {
     public function completeLesson($lesson) {
         Redis::sadd("user:{$this->id}:series:{$lesson->series->id}", $lesson->id);
     }
+
+    /**
+     * Get percentage completed for a series for a user
+     *
+     * @param [App\Series] $series
+     * @return void
+     */
+    public function percentageCompletedForSeries($series) {
+        $numberOfLessonsInSeries = $series->lessons->count();
+        $numberOfCompletedLessons = $this->getNumberOfCompletedLessonsForASeries($series);
+
+        return ($numberOfCompletedLessons / $numberOfLessonsInSeries) * 100;
+    }
+
+    /**
+     * Get number of completed lessons for a series
+     *
+     * @param [App\Series] $series
+     * @return void
+     */
+    public function getNumberOfCompletedLessonsForASeries($series) {
+        return count($this->getCompletedLessonsForASeries($series));
+    }
+
+    /**
+     * Check if a user has started a series
+     *
+     * @param [App\Series] $series
+     * @return boolean
+     */
+    public function hasStartedSeries($series) {
+        return $this->getNumberOfCompletedLessonsForASeries($series) > 0;
+    }
+
+    /**
+     * Get array of completed lessons ids for a series
+     *
+     * @param [App\Series] $series
+     * @return array
+     */
+    public function getCompletedLessonsForASeries($series) {
+        return Redis::smembers("user:{$this->id}:series:{$series->id}");
+    }
+
+    /**
+     * Get all completed lessons for a series
+     *
+     * @param [App\Series] $series
+     * @return \Illuminate\Support\Collection(App\Lesson)
+     */
+    public function getCompletedLessons($series) {
+        // 1, 2, 4
+        return Lesson::whereIn('id', 
+            $this->getCompletedLessonsForASeries($series)
+        )->get();
+    }
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -26,6 +26,7 @@ class UserTest extends TestCase
         
         $user = factory(User::class)->create();
         $lesson = factory(Lesson::class)->create();
+
         $lesson2 = factory(Lesson::class)->create([
             'series_id' => 1
         ]);
@@ -37,9 +38,73 @@ class UserTest extends TestCase
             [1, 2]
         );
 
-        // $this->assertEquals(
-        //     $user->getNumberOfCompletedLessonsForASeries($lesson->series),
-        //     2
-        // );
+        $this->assertEquals(
+            $user->getNumberOfCompletedLessonsForASeries($lesson->series),
+            2
+        );
+    }
+
+    public function test_can_get_percentage_completed_for_series_for_a_user() {
+        
+        $this->flushRedis();
+        $user = factory(User::class)->create();
+        $lesson = factory(Lesson::class)->create();
+        factory(Lesson::class)->create(['series_id' => 1]);
+        factory(Lesson::class)->create(['series_id' => 1]);
+        $lesson2 = factory(Lesson::class)->create([
+            'series_id' => 1
+        ]);
+        $user->completeLesson($lesson);
+        $user->completeLesson($lesson2);
+
+        $this->assertEquals(
+            $user->percentageCompletedForSeries($lesson->series),
+            50
+        );
+    }
+
+    public function test_can_know_if_a_user_has_started_a_series() {
+        //user, 2 series,
+        $this->flushRedis();
+        $user = factory(User::class)->create();
+        $lesson = factory(Lesson::class)->create();
+        $lesson2 = factory(Lesson::class)->create([
+            'series_id' => 1
+        ]);
+        $lesson3 = factory(Lesson::class)->create();
+
+        //user watches a lesson in the 1st series
+        $user->completeLesson($lesson2);
+
+        // assert that returns true hasStartedseries(1)
+        $this->assertTrue($user->hasStartedSeries($lesson->series));
+        $this->assertFalse($user->hasStartedSeries($lesson3->series));
+    }
+
+    public function test_can_get_completed_lessons_for_a_series() {
+        // user , series
+        $this->flushRedis();
+
+        $user = factory(User::class)->create();
+        $lesson = factory(Lesson::class)->create();
+        $lesson2 = factory(Lesson::class)->create([ 'series_id' => 1 ]);
+        $lesson3 = factory(Lesson::class)->create([ 'series_id' => 1 ]);
+
+        //completes some lessons in the series
+        $user->completeLesson($lesson);
+        $user->completeLesson($lesson2);
+
+        //get completed lessons method 
+        $completedLessons = $user->getCompletedLessons($lesson->series);
+        
+        //assert that the lessons completed are in the return value 
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $completedLessons);
+        $this->assertInstanceOf(Lesson::class, $completedLessons->random());
+
+        $completedLessonsIds = $completedLessons->pluck('id')->all();
+
+        $this->assertTrue(in_array($lesson->id, $completedLessonsIds));
+        $this->assertTrue(in_array($lesson2->id, $completedLessonsIds ));
+        $this->assertFalse(in_array($lesson3->id, $completedLessonsIds));
     }
 }
